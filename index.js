d1feb6e2362e3b24b94a80badc4cb7fd3294432b
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-03-28 13:44:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+alert('color change hoga')
+console.warn('side mein dekh')
+function red(){
+		document.getElementById('header').style.color='red';
+	}
+function green(){
+		document.getElementById('header').style.color='green';
+	}
